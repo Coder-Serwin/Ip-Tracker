c04fda5db6d636f08de53701930f79a7e3565a45
Coder-Serwin <roughuseforaronserwin@gmail.com>
2022-12-12 16:36:26 +0530

    new files

--- ip.py
+++ ip.py
@@ -1,11 +1,14 @@
 from flask import Flask, request
+from bot import *
+from env import D_TOKEN
 
 app = Flask(__name__)
-
+ip_address = None
 
 @app.route('/')
 def home():
     ip_address = request.remote_addr
+    client.run(D_TOKEN)
     return f"{ip_address}"
 
 
