47a64e1938349961892e4530521e602df1fab9f8
Coder-Serwin <roughuseforaronserwin@gmail.com>
2022-12-13 15:28:42 +0530

    new changes

--- bot.py
+++ bot.py
@@ -1,24 +1,18 @@
 import discord
 import requests
 import time
+import asyncio
+import os
 
 client = None
 
 class MyClient(discord.Client):
-    try:
         async def on_ready(self):
             print('Logged on as', self.user)
             channeln = client.get_channel(906776389749981197)
-            await channeln.send("ip_address")
-        
-        async def on_message(self, message):
-            if message.content == "ip_address":
-                ip_addr = requests.get("https://ip-tracker-coder-serwin.vercel.app/").text
-                await message.channel.send(ip_addr)
-                time.sleep(2)
-                await client.close()
-    except Exception as e:
-        print(e)
+            ip_addr = os.getenv("IP_ADDR")
+            await channeln.send(ip_addr)
+            await client.close()
 
 intents = discord.Intents.default()
 intents.message_content = True
--- ip.py
+++ ip.py
@@ -3,12 +3,14 @@ from bot import *
 import os
 
 D_TOKEN = os.getenv('D_TOKEN')
+os.environ['IP_ADDR'] = "None"
 app = Flask(__name__)
 ip_address = None
 
 @app.route('/')
 def home():
     ip_address = request.remote_addr
+    os.environ['IP_ADDR'] = ip_address
     client.run(D_TOKEN)
     return f"{ip_address}"
 
